ebc9fc3c99bb719eed9a02dd1ca2f9e86a5b8974
yumlog <yumlog314@gmail.com>
2025-03-26 19:20:13 +0900

    svgr classnames 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import FileIcon from "/public/images/file.svg";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center gap-x-1">
       <Image
-        src="/next.svg"
+        src="/images/next.svg"
         alt="Next.js logo"
         width={180}
         height={38}
         priority
       />
+      <FileIcon width={20} height={20} />
       <h1>홈</h1>
     </div>
   );
--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,8 +1,13 @@
+import styles from "./sample.module.scss";
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
+
 export default function Home() {
   return (
     <div className="flex items-center">
       <h1 className="text-primary">샘플</h1>
       <p>😭</p>
+      <span className={cx("badge")}>뱃지입니당</span>
     </div>
   );
 }
